d6dfc6ea1fd5d81b8f245d814cc008b8b3113b20
kins <k1dkins@yahoo.com>
2023-05-01 09:19:29 -0400

    feat: add conditional logic to check for existing files

--- genproj.bat
+++ genproj.bat
@@ -1,11 +1,32 @@
 @echo off
-md %1\build
+if exist %1\build\ (
+	echo build folder not created. This directory already exists.
+) else (
+	md %1\build
+)
 pushd %1
-md data\logs
+if exist data\logs\ (
+	echo logs folder not created. This directory already exists.
+) else (
+	md data\logs
+)
 popd
 pushd %1
-md src
+if exist src\ (
+	echo src folder not created. This directory already exists
+) else (
+	md src
+)
 popd
 
-type templates\log_template.txt >> %1\data\logs\log.txt
-type templates\main_template.cpp >> %1\src\main.cpp
+if exist %1\data\logs\log.txt (
+	echo log file already exists.
+) else (
+	type templates\log_template.txt >> %1\data\logs\log.txt
+)
+
+if exist %1\src\main.cpp (
+	echo main file already exists.
+) else (
+	type templates\main_template.cpp >> %1\src\main.cpp
+)
